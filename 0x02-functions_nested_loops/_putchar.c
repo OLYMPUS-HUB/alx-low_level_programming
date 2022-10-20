ee195cb8f4cd315e43a72be7e8c44540418fd319
buruboyefe samson <sammycares66@gmail.com>
2022-10-19 18:30:04 -0700

    task0  nested

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "unistd.h"
+
+/**
+ * _putchar - it writes the character c to stdout
+ * @c: the character to print
+ * olmypus is here
+ * Return: On success 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
