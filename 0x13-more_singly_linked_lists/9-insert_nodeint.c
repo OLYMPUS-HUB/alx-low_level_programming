7504c8cb3be2208e220622b1bcb792224f86d07b
buruboyefe samson <sammycares66@gmail.com>
2022-11-29 01:30:45 -0800

    task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,63 @@
+
+samibirhanu1
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+1
+Actions
+Projects
+More
+alx-low_level_programming/0x13-more_singly_linked_lists/9-insert_nodeint.c
+@samibirhanu1
+samibirhanu1 electri city
+ History
+ 1 contributor
+45 lines (37 sloc)  960 Bytes
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *copy = *head;
+	unsigned int node;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = copy;
+		*head = new;
+		return (new);
+	}
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
+
+		copy = copy->next;
+	}
+
+	new->next = copy->next;
+	copy->next = new;
+
+	return (new);
+}
